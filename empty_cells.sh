8f4a1494113fce6c560ed32613171ddbda43ebc8
ManasR10 <manasrawat1234@gmail.com>
2025-05-18 18:52:31 +0800

    Add empty_cells.sh

---
+++ empty_cells.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# empty_cells.sh: Count empty cells in each column of a delimited file
+# Usage: empty_cells.sh <input-file> <separator>
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <input-file> <separator>" >&2
+  exit 1
+fi
+
+file="$1"
+sep="$2"
+
+awk -v FS="$sep" 'NR==1 {
+    n = NF
+    for (i = 1; i <= NF; i++) {
+        header[i] = $i
+        counts[i] = 0
+    }
+    next
+}
+{
+    for (i = 1; i <= n; i++) {
+        if ($i == "") counts[i]++
+    }
+}
+END {
+    for (i = 1; i <= n; i++) {
+        print header[i]": "counts[i]
+    }
+}' "$file"
+```
